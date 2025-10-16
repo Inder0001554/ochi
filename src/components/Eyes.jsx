a6aacb27327e495d9f5a75caafa9a424d1b76272
inder <ij653588@gmail.com>
2025-10-16 09:44:01 +0530

    responsive

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -1,42 +1,59 @@
 import React, { useEffect, useState } from 'react'
 
 function Eyes() {
-  const [rotate, setRotate] = useState(0)
-  useEffect(()=>{
-    window.addEventListener("mousemove", (e)=>{
-      let  mouseX = e.clientX;
-      let  mouseY = e.clientY;
+  const [rotate, setRotate] = useState(0);
 
-      let deltaX = mouseX - window.innerWidth/2;
-      let deltaY = mouseY - window.innerHeight/2;
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
 
-      var angle = Math.atan2(deltaX, deltaY) * (180/Math.PI);
-      setRotate(angle-180);
-    })
-  })
+      const deltaX = mouseX - window.innerWidth / 2;
+      const deltaY = mouseY - window.innerHeight / 2;
+
+      const angle = Math.atan2(deltaX, deltaY) * (180 / Math.PI);
+      setRotate(angle - 180);
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
   return (
     <div className='eyes w-full h-screen overflow-hidden'>
-      <div data-scroll data-scroll-speed="-.7" className='relative w-full h-full bg-[url(https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg)] bg-cover bg-center'>
-      <div className='absolute flex gap-10  top-1/2 left-1/2  -translate-x-[50%] -translate-y-[50%] '>
-          <div className='flex items-center justify-center w-[15vw] h-[15vw] bg-zinc-100 rounded-full'>
-             <div className='relative items-center justify-center w-2/3 h-2/3 bg-zinc-900 rounded-full'>
-             <div style={{ transform: `translate(-50%, -50%) rotate(${rotate}deg)`}} className=' mt-4 ml-16 line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-8'>
-
-              <div className='w-10 h-10 bg-zinc-100 rounded-full'></div>
-            </div>
+      <div
+        data-scroll
+        data-scroll-speed="-.7"
+        className='relative w-full h-full bg-[url(https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg)] bg-cover bg-center'
+      >
+        <div className='absolute flex flex-col sm:flex-row gap-6 sm:gap-10 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]'>
           
-          </div>
-          </div>
-          <div className='flex items-center justify-center w-[15vw] h-[15vw] bg-zinc-100 rounded-full'>
-            <div className='relative items-center justify-center w-2/3 h-2/3 bg-zinc-900 rounded-full'>
-              <div style={{ transform: `translate(-50%, -50%) rotate(${rotate}deg)` }} className=' mt-4 ml-16 line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-8'>
-
-                <div className='w-10 h-10 bg-zinc-100 rounded-full'></div>
+          {/* Left Eye */}
+          <div className='flex items-center justify-center w-[35vw] h-[35vw] sm:w-[20vw] sm:h-[20vw] md:w-[15vw] md:h-[15vw] bg-zinc-100 rounded-full'>
+            <div className='relative flex items-center justify-center w-2/3 h-2/3 bg-zinc-900 rounded-full'>
+              <div
+                style={{ transform: `translate(-50%, -50%) rotate(${rotate}deg)` }}
+                className='mt-4 ml-10 sm:ml-14 md:ml-16 line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-8'
+              >
+                <div className='w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 bg-zinc-100 rounded-full'></div>
               </div>
             </div>
-
           </div>
-      </div>
+
+          {/* Right Eye */}
+          <div className='flex items-center justify-center w-[35vw] h-[35vw] sm:w-[20vw] sm:h-[20vw] md:w-[15vw] md:h-[15vw] bg-zinc-100 rounded-full'>
+            <div className='relative flex items-center justify-center w-2/3 h-2/3 bg-zinc-900 rounded-full'>
+              <div
+                style={{ transform: `translate(-50%, -50%) rotate(${rotate}deg)` }}
+                className='mt-4 ml-10 sm:ml-14 md:ml-16 line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-8'
+              >
+                <div className='w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 bg-zinc-100 rounded-full'></div>
+              </div>
+            </div>
+          </div>
+
+        </div>
       </div>
     </div>
   )
